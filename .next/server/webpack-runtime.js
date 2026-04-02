821848542d2ba248cedab950152c166740392675
Maximilian019 <maximilian.schranzlageder@gmail.com>
2026-04-02 20:06:28 +0200

    clean next build

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("450e0279cab26a6b")
+/******/ 		__webpack_require__.h = () => ("da576a69f240bebd")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8748fc54967c6d1f"; }
+/******/ 		__webpack_require__.h = function() { return "0855a250b6a16794"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
